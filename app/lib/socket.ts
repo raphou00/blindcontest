b53b9b7cefb3b01ea0d400b01b0031b3085532fe
Raphaël <105916427+raph00000@users.noreply.github.com>
2023-11-22 01:13:12 +0100

    Update server port

--- app/lib/socket.ts
+++ app/lib/socket.ts
@@ -1,6 +1,6 @@
 import io, { Socket } from "socket.io-client";
 
-const server: string = "https://162.19.243.4:3000";
+const server: string = "https://162.19.243.4:3001";
 const socket: Socket = io(server);
 
 export default socket;
